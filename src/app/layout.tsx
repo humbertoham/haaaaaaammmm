c3aa6a5d229c6ccf48aadb5c60a998a1e3375f95
humbertoham <hamduranhumberto@gmail.com>
2025-10-14 00:14:06 -0600

    theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <meta name="theme-color" content="#000000" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
