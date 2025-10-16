4623ee551c4dd729e47480dbdbf63fa374af45c3
humbertoham <hamduranhumberto@gmail.com>
2025-10-16 00:53:44 -0600

    yo ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,31 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 
-
-export const metadata: Metadata = {
+export const metadata = {
   title: "yo",
-  description: ":ppp",
+  description: ":pp",
+  openGraph: {
+    title: "yo",
+    description: ":pp",
+    url: "https://haaaaaaammmm.com", // ← cambia por tu dominio real
+    siteName: "yo",
+    images: [
+      {
+        url: "/images/YO.jpeg", // ← pon tu imagen en /public/og-image.jpg
+        width: 1200,
+        height: 630,
+        alt: "yo Preview",
+      },
+    ],
+    locale: "es_ES",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "yo",
+    description: ":pp",
+    images: ["/images/YO.jpeg"],
+  },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Link from "next/link";
-
+import Image from "next/image";
 export default function Home() {
   return (
   <div className="bg-white m-20">
-    <Link href="/deathclock" className=" hover:underline text-blue-500 hover:text-blue-700">deathclock</Link>
+    <a href="/deathclock" className=" hover:underline text-blue-500 hover:text-blue-700 block">deathclock</a>
+    <a href="https://www.instagram.com/haaaaaaaaaammmm" className=" hover:underline text-blue-500 hover:text-blue-700 block">instagram</a>
+    <a href="https://x.com/haaaaaaaammmmm" className=" hover:underline text-blue-500 hover:text-blue-700 block">twitter</a>
+    <Image src="/images/VISANAME.png" alt="name" width={300} height={300}/>
   </div>
   );
 }
